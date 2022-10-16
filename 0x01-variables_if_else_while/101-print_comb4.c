c40db4c37b90184c22359769fc94aacf71340beb
ken-001-mk <mwaskenne@gmail.com>
2022-10-16 10:32:57 -0700

    combination of three numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - print different combinations of three digits
+ * You can only use putchar six times maximum in your code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int y, x, z;
+
+	for (y = 0; y < 10; y++)
+	{
+		for (x = y + 1; x < 10; x++)
+		{
+			for (z = x + 1; x < 10; x++)
+			{
+				putchar((y % 10) + '0');
+				putchar((x % 10) + '0');
+				putchar((z % 10) + '0');
+
+				if (y == 7 && x == 8 && z == 9)
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
