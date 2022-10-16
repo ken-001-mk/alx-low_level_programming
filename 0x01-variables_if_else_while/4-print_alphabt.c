1e94fe6384f1783abdace4e6a214166a7db9004b
ken-001-mk <mwaskenne@gmail.com>
2022-10-16 03:09:37 -0700

    lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print all letters but the letter 'q' and 'e'
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
