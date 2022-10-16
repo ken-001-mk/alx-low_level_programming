c8aad30a9d0e44daf558e95b829c9d0e1f7b2c0f
ken-001-mk <mwaskenne@gmail.com>
2022-10-16 03:34:27 -0700

    number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print digit numbers from 0 - 10
+ * Return: 0
+ */
+int main(void)
+{
+	int y;
+
+	for (y = 0; y < 10; y++)
+	{
+		printf("%d", y);
+	}
+	printf("\n");
+
+	return (0);
+}
