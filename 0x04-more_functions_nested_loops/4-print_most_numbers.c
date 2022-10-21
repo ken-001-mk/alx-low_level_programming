7fb36f5033876fd65490b30478b12cd98cf60420
ken-001-mk <mwaskenne@gmail.com>
2022-10-21 12:12:39 -0700

    more numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,14 +7,16 @@
  */
 void print_most_numbers(void)
 {
-	int i;
 
-	for (i = 0; i <= 9; i++)
+	int num;
+
+	for (num = 0; num <= 9; num++)
 	{
-		if ((i == 2) || (i == 4))
-			continue;
-		else
-			_putchar(i + '0');
+		if (num != 2 && num != 4)
+			_putchar(num + '0');
+
+		num++;
 	}
+
 	_putchar('\n');
 }
