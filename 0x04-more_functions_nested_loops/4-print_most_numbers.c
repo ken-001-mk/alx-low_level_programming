683fcf464cb5fa985a3601407d78bbd4498b9de5
ken-001-mk <mwaskenne@gmail.com>
2022-10-23 11:31:07 -0700

    more numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_most_numbers - a function that prints the numbers, from 0 to 9
  *
- * Return: 0-9, excluding 2 and 4, followed by new line
+ * Return: void
  */
 void print_most_numbers(void)
 {
