a10d863fb291ed92de022b71febfe595ac8a1d08
ken-001-mk <mwaskenne@gmail.com>
2022-11-03 13:53:58 -0700

    reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,14 +2,14 @@
 
 /**
 * _print_rev_recursion - prints a string in reverse
-*@s: string to be printed
+*@s: string to be printed in reverse
 */
 
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_putchar(*s);
 		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
