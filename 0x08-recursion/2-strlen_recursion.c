a6a9097e725008a735c0482c021d237f9c2a42b2
ken-001-mk <mwaskenne@gmail.com>
2022-11-04 10:47:51 -0700

    string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _strlen_recursion - returns the length of a string
+* @s: length of a string to be printed
+* Return: length of a string
+*/
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+	else
+	{
+		return (len);
+	}
+}
